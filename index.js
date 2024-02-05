1ca47a49e23ce4118cb39e0a669bdbed66b86311
Marcos Maia <henriquemarcosmaia@gmail.com>
2024-02-05 20:11:43 -0300

    fix: removing bar from begin of images source

--- index.js
+++ index.js
@@ -9,29 +9,159 @@ const $buttonScissorsPlayer2 = document.querySelector('.button-move-scissors-2')
 const $moveBox1 = document.querySelector('#move-box-1')
 const $moveBox2 = document.querySelector('#move-box-2')
 
+const $scorePlayer1 = document.querySelector('#score-1')
+const $scorePlayer2 = document.querySelector('#score-2')
+
+const $winnerTitle = document.querySelector('.winner-title')
+
+let movePlayer1 = ''
+let movePlayer2 = ''
+let winner = 0
+let player1Score = 0
+let player2Score = 0
+
+function setWinner(){
+    if (movePlayer1 == '' || movePlayer2 == '') {
+        return
+    }
+
+    if (movePlayer1 == 'stone' && movePlayer2 == 'paper') {
+        winner = 2
+    } else if (movePlayer1 == 'stone' && movePlayer2 == 'scissors') {
+        winner = 1
+    } else if (movePlayer1 == 'paper' && movePlayer2 == 'stone') {
+        winner = 1
+    } else if (movePlayer1 == 'paper' && movePlayer2 == 'scissors') {
+        winner = 2
+    } else if (movePlayer1 == 'scissors' && movePlayer2 == 'stone') {
+        winner = 2
+    } else if (movePlayer1 == 'scissors' && movePlayer2 == 'paper') {
+        winner = 1
+    } else if (movePlayer1 == 'stone' && movePlayer2 == 'paper') {
+        winner = 2
+    } else if (movePlayer1 == movePlayer2) {
+        winner = 3
+    }
+}
+
+function addWinnerScore(){
+    if (winner == 1) {
+        player1Score = player1Score + 1
+    } else if (winner == 2) {
+        player2Score = player2Score + 1
+    }
+}
+
+function printWinnerScore(){
+    $scorePlayer1.innerHTML = player1Score.toString().padStart(2, '0')
+    $scorePlayer2.innerHTML = player2Score.toString().padStart(2, '0')
+}
+
+function printWinnerName() {
+    if (winner == 1) {
+        $winnerTitle.innerHTML = 'Jogador 1 venceu'
+    }
+    if (winner == 2) {
+        $winnerTitle.innerHTML = 'Jogador 2 venceu'
+    }
+    if (winner == 3) {
+        $winnerTitle.innerHTML = 'Empatou'
+    }
+}
+
+function resetBattlefield() {
+    $moveBox1.innerHTML = ''
+    $moveBox2.innerHTML = ''
+}
+
+function resetMoveVariables(){
+    movePlayer1 = ''
+    movePlayer2 = ''
+}
+
 function handleStone1Move() {
-    $moveBox1.innerHTML = '<img src="/images/stone.png" alt="imagem pedra" title="imagem pedra">'
+    $moveBox1.innerHTML = '<img src="images/stone.png" alt="imagem pedra" title="imagem pedra">'
+    movePlayer1 = 'stone'
+    setWinner()
+    addWinnerScore()
+    printWinnerScore()
+    printWinnerName()
+    if (winner != 0) {
+        setTimeout(resetBattlefield, 1000)
+        resetMoveVariables()
+        winner = 0
+    }
 }
 
 function handlePaper1Move() {
-    $moveBox1.innerHTML = '<img src="/images/paper.png" alt="imagem papel" title="imagem papel">'
+    $moveBox1.innerHTML = '<img src="images/paper.png" alt="imagem papel" title="imagem papel">'
+    movePlayer1 = 'paper'
+    setWinner()
+    addWinnerScore()
+    printWinnerScore()
+    printWinnerName()
+    if (winner != 0) {
+        setTimeout(resetBattlefield, 1000)
+        resetMoveVariables()
+        winner = 0
+    }
 }
 
 function handleScissors1Move() {
-    $moveBox1.innerHTML = '<img src="/images/scissors.png" alt="imagem tesoura" title="imagem tesoura">'
+    $moveBox1.innerHTML = '<img src="images/scissors.png" alt="imagem tesoura" title="imagem tesoura">'
+    movePlayer1 = 'scissors'
+    setWinner()
+    addWinnerScore()
+    printWinnerScore()
+    printWinnerName()
+    if (winner != 0) {
+        setTimeout(resetBattlefield, 1000)
+        resetMoveVariables()
+        winner = 0
+    }
 }
 
 
 function handleStone2Move() {
-    $moveBox2.innerHTML = '<img src="/images/stone.png" alt="imagem pedra" title="imagem pedra">'
+    $moveBox2.innerHTML = '<img src="images/stone.png" alt="imagem pedra" title="imagem pedra">'
+    movePlayer2 = 'stone'
+    setWinner()
+    addWinnerScore()
+    printWinnerScore()
+    printWinnerName()
+    if (winner != 0) {
+        setTimeout(resetBattlefield, 1000)
+        resetMoveVariables()
+        winner = 0
+    }
 }
 
 function handlePaper2Move() {
-    $moveBox2.innerHTML = '<img src="/images/paper.png" alt="imagem papel" title="imagem papel">'
+    $moveBox2.innerHTML = '<img src="images/paper.png" alt="imagem papel" title="imagem papel">'
+    movePlayer2 = 'paper'
+    setWinner()
+    addWinnerScore()
+    printWinnerScore()
+    printWinnerName()
+    if (winner != 0) {
+        setTimeout(resetBattlefield, 1000)
+        resetMoveVariables()
+        winner = 0
+    }
 }
 
 function handleScissors2Move() {
-    $moveBox2.innerHTML = '<img src="/images/scissors.png" alt="imagem tesoura" title="imagem tesoura">'
+    $moveBox2.innerHTML = '<img src="images/scissors.png" alt="imagem tesoura" title="imagem tesoura">'
+    movePlayer2 = 'scissors'
+    setWinner()
+    addWinnerScore()
+    printWinnerScore()
+    printWinnerName()
+    if (winner != 0) {
+        setTimeout(resetBattlefield, 1000)
+        resetMoveVariables()
+        winner = 0
+    }
 }
 
 $buttonStonePlayer1.addEventListener('click', handleStone1Move) 
